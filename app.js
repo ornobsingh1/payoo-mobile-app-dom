afcc97ed75c8594c919e3d7d3d48e09a997e1de5
Ornob Singh <ornobsingh14@gmail.com>
2025-08-25 13:03:42 +0600

    adding lohin functionality and redirect to home

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 // login button funtionality
 document.getElementById("btn-login").addEventListener("click", function (e) {
   e.preventDefault();
-  console.log("login btn click");
+
+  const mobileNumber = 12345678910;
+  const pinNumber = 1234;
+
+  const mobileNumberValue = document.getElementById("mobile-number").value;
+  const mobileNumberValueConverted = parseInt(mobileNumberValue);
+
+  const pinNumberValue = document.getElementById("pin-number").value;
+  const pinNumberValueConverted = parseInt(pinNumberValue);
+
+  if (
+    mobileNumberValueConverted === mobileNumber &&
+    pinNumberValueConverted === pinNumber
+  ) {
+    window.location.href = "./home.html";
+  } else {
+    alert("Invalid credentials");
+  }
 });
