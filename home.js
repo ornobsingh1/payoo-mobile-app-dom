1d3b8537c46b12980e246f19f461c8456a73b2d1
Ornob Singh <ornobsingh14@gmail.com>
2025-08-27 01:12:52 +0600

    implement pin feature

--- home.js
+++ home.js
@@ -1,14 +1,23 @@
+const validPin = 1234;
+
 document.getElementById("add-money").addEventListener("click", function (e) {
   e.preventDefault();
 
   const bank = document.getElementById("bank").value;
   const accountNumber = document.getElementById("account-number").value;
   const amount = parseInt(document.getElementById("add-amount").value);
-  const pin = document.getElementById("add-pin").value;
+  const pin = parseInt(document.getElementById("add-pin").value);
 
   const avaliableBalance = parseInt(
     document.getElementById("avaliable-balance").innerText
   );
+
+  if (accountNumber.length < 11) {
+    return alert("Please Provide Valid Account Number");
+  }
+  if (pin !== validPin) {
+    return alert("Please Provide Valid Pin Number");
+  }
 
   const totalNewAvailableBalance = amount + avaliableBalance;
   document.getElementById("avaliable-balance").innerText =
